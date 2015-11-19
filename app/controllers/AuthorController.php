4438268027ff4cbaef448b6c7d17d95a68783fa0
nicolas.breynaert <nicolas.breynaert@sts-sio-caen.info>
2015-11-19 12:08:30 +0100

    implemntation progress bar + calcul jour

--- app/controllers/AuthorController.php
+++ app/controllers/AuthorController.php
@@ -1,5 +1,6 @@
 <?php
 use Ajax\bootstrap\html\html5\HtmlSelect;
+use Ajax\ui\Components\Progressbar;
 class AuthorController extends DefaultController{
 	public function initialize(){
 		parent::initialize();
@@ -8,6 +9,8 @@ class AuthorController extends DefaultController{
 	public function ProjectsAction($id=NULL){
 		$user=User::findFirst("id=".$id);
  		$p=Projet::find("idAuthor=".$id);
+		$this->jquery->bootstrap()->htmlProgressbar("pb1","success",80)->setStriped(true)->setActive(true)->showcaption(true);
+		$this->jquery->compile($this->view);
 		$this->view->setVars(array("user"=>$user, "projects"=>$p));
 	}
 }
--- app/views/author/projects.volt
+++ app/views/author/projects.volt
@@ -1,8 +1,19 @@
 <html>
 	<legend>Mes projets : [{{ user.getIdentite() }}]</legend>
 	<table class='table table-striped'>
+		<thead>
+		</thead>
+		<tbody>
 		{% for project in projects %}
-			<tr><td>{{project.toString()}}</td></tr>
+			<tr>
+				<td>{{project.toString()}}</td>
+				<td class="col-md-7">
+					{{q["pb1"]}}
+				</td>
+				<td class="td-center">Reste {{project.getJourRestant()}} jours</td>
+				<td class="td-center"><a class="btn btn-primary" href="">Ouvrir...</a></td>
+			</tr>
 		{% endfor %}
+		</tbody>
 	</table>
 </html>
